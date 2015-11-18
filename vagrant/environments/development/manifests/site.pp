53f7079b4a0df7f4b47e69ccfaaf382833f2ba28
Alex Casajuana <alexcm.14@gmail.com>
2015-11-18 20:12:34 +0100

    Commit inicial

---
+++ vagrant/environments/development/manifests/site.pp
@@ -0,0 +1,1 @@
+include alex_module
---
+++ vagrant/scripts/install_modules.sh
@@ -0,0 +1,16 @@
+dir='/vagrant/modules/'
+
+puppet module install example42/yum --modulepath $dir --version 2.1.25;
+puppet module install example42/php  --modulepath $dir --version 2.0.25;
+puppet module install puppetlabs-apache --modulepath $dir --version 1.6.0;
+puppet module install puppetlabs/mysql --modulepath $dir --version 3.6.1;
+puppet module install puppetlabs-mongodb --modulepath $dir --version 0.11.0;
+puppet module install example42/memcached --modulepath $dir --version 3.0.5;
+puppet module install crayfishx/firewalld --modulepath $dir --version 1.2.3;
+puppet module install example42-timezone --modulepath $dir --version 2.0.13;
+puppet module install example42/ntp --modulepath $dir --version 2.0.15;
+
+yum install -y git
+git clone <mi repo> /vagrant/modules/
+
+exit 0;
